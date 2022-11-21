70043a817bc160c442c525bfc73f22d7147d9904
paigexc70 <catherinep1090@gmail.com>
2022-11-20 20:59:01 -0600

    fix

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ const {MongoClient} = require('mongodb');
 
 async function main(){
 
-    const uri = 'mongodb+srv://paiged:<G2m2YxdGCOkBbFUY>@cluster0.9jk8vwf.mongodb.net/?retryWrites=true&w=majority';
+    const uri = "mongodb+srv://paiged:G2m2YxdGCOkBbFUY@cluster0.9jk8vwf.mongodb.net/?retryWrites=true&w=majority";
 
     const client = new MongoClient(uri);
 
